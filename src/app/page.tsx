6d0bd89095dd0a1ba1c0dccfaa496a3312cc32ae
Pedro <pedro_neto@outlook.com>
2024-09-21 15:05:12 -0300

    use auth on first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
@@ -14,19 +15,34 @@ const mockImages = mockUrl.map((url, index) => ({
   url,
 }));
 
+export async function Images() {
+  const images = await db.query.images.findMany();
+
+  return (
+    <div className="flex flex-col gap-4">
+      {images.map((image, index) => (
+        <div key={image.id + "-" + index} className="w-48">
+          <img src={image.url} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 export default async function HomePage() {
   const images = await db.query.images.findMany();
 
   return (
     <main className="">
-      <div className="flex flex-col gap-4">
-        {images.map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
