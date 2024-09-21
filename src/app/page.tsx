2f7ac9a5743e10f192e3678858ec80ef87eea707
Pedro <pedro_neto@outlook.com>
2024-09-21 14:29:54 -0300

    update database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,19 +24,17 @@ function TopNav() {
 }
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany();
 
   return (
     <main className="">
       <TopNav />
 
       <div className="flex flex-col gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {mockImages.map((image, index) => (
+        {images.map((image, index) => (
           <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
