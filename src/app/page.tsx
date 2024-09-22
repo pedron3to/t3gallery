4509c78ec2e3900a1a8847cb77146ffa88d47b40
Pedro <pedro_neto@outlook.com>
2024-09-21 23:00:18 -0300

    query into separated files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,15 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
+import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
-const mockUrl = [
-  "https://utfs.io/f/9PQTfydC5wYyROnx9bmUBWNcbi80tMygHZCYGlPhfE43e6xo",
-  "https://utfs.io/f/9PQTfydC5wYyA0QUAS97qMwaAS390ZNXnDUkyLWKhv61i2ed",
-  "https://utfs.io/f/9PQTfydC5wYynLhXbbTbAa9tXEUxkdijJzuKPhYvf50qFyOC",
-  "https://utfs.io/f/9PQTfydC5wYy6iKA5PqBzcYxZdSer8bko3s2w1UWmAHDtEVv",
-];
-
-const mockImages = mockUrl.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export async function Images() {
-  const images = await db.query.images.findMany();
+  const images = await getMyImages();
 
   return (
     <div className="flex flex-wrap gap-4">
-      {images.map((image) => (
+      {images?.map((image) => (
         <div key={image.id} className="w-48">
           <img src={image.url} />
           <div>{image.name}</div>
