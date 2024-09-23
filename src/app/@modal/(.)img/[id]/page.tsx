e3f8427f0dfa02fe04a7fa2e00baee3540d03d56
Pedro <pedro_neto@outlook.com>
2024-09-22 22:16:09 -0300

    fixed dialog fullscreen

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,17 +1,12 @@
-import { getImage } from "~/server/queries";
+import FullPageImageView from "~/components/full-image-page";
 
-export default async function PhotoModal({
+export default function PhotoPage({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
   const idAsNumber = Number(photoId);
   if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
-  const image = await getImage(idAsNumber);
 
-  return (
-    <div>
-      <img src={image?.url} className="h-96" />
-    </div>
-  );
+  return <FullPageImageView id={idAsNumber} />;
 }
